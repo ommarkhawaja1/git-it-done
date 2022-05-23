50a5977300223b938b105259966a0f3b88f7df27
ommarkhawaja1 <ommarkhawaja1@gmail.com>
2022-05-22 22:40:17 -0400

    get names of issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,24 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if(repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  }
+  else {
+    // if no repo was given, redirect tot he homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -20,8 +38,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
@@ -44,7 +61,6 @@ var displayIssues = function(issues) {
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
     
-  
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -81,4 +97,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName()
